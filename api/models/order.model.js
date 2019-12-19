ed4a53c71b1d6ddbbe3e153dfd2069e245fb8889
NgoMinhNhi <ngominhnhi1998@gmail.com>
2019-12-20 02:41:15 +0700

    fix api

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema;
 
 const Order = new Schema({
   userId: {type: String, required: true, index: 1},
+  phoneNumber: {type: String},
   address: { type: String },
   timeOrder: { type: Date, default: Date.now},
   timeService: { type: Date, },
   listItem: { type: Array},
-  status: {type: String, enum: ['wait-confirm', 'doing', 'service', 'reject'], default: 'wait-confirm'},
+  status: {type: String, enum: ['new', 'seen', 'confirm', 'doing', 'service', 'reject'], default: 'new'},
 });
 
 export default mongoose.model('order', Order);
